90ed1315506d05f4f1449691d1708a5ff40e7776
Grant McGovern <grant@cobrain.com>
2015-04-03 21:06:58 -0400

    Small fix suggested by Dr. John

--- Squareroot/Lab/babylonian.s
+++ Squareroot/Lab/babylonian.s
@@ -1,8 +1,16 @@
+/*
+*
+*
+* Author: Grant McGovern
+* Date: 1 April 2015
+*
+*/
 
 .global babylonian
 
 .text
 
+/* Macros */
 .macro fmuls rgC, rgB, rgA
 	custom 252, \rgC, \rgB, \rgA
 .endm
@@ -19,26 +27,47 @@
 	custom 255, \rgC, \rgB, \rgA
 .endm
 
+/* Note: I decided to use register 2 (r2) as a register througout the program
+	rather than using an additional register. I know it is unconventional to
+	use the return r2 register like this, but I found it made things simpler.
 
+*/
 babylonian:
-
+	/* Move 2.0 into r6 */
 	movia r6, FTwo
+	/* Load the value */
+	
 	ldw r6, 0(r6)
+	
+	/* Divide r4 by r6 and store it in r2 */
 	fdivs r2, r4, r6                                 
 
-doloop:
-	mov r3, r2
+loop:
+	/* Put r2 into r3 */
+	add r3, r0, r2
+
+	/* Perform a series of floating point arithmetic using above macros */
 	fdivs r2, r4, r3
 	fadds r2, r2, r3
-	fdivs r2, r4, r6
+	
+	/* Each of these steps mimicks the algorithm */
+	fdivs r2, r2, r6
+	
 	fsubs r5, r2, r3
+	
 	fdivs r5, r2, r3
 	fdivs r5, r5, r2 	
 
+	/* Perform bitwise operations as discussed in class */
 	srli r5, r5, 23
+	
+	/* Perform a logical and imediate */
 	andi r5, r5, 0x00ff
+	
 	cmpgti r5, r5, 0x0068
-	bgt r5, r0, doloop
+	
+	/* Go back to the loop top */
+	bgt r5, r0, loop
 	
 
 ret
